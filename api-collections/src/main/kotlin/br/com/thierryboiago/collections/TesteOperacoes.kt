63e8ad16b306cbaefb564de5a8d02d24c0ea7d1e
Thierry <thierry123123@gmail.com>
2021-07-22 22:14:18 -0300

    criado o teste de Operações do array

---
+++ api-collections/src/main/kotlin/br/com/thierryboiago/collections/TesteOperacoes.kt
@@ -0,0 +1,34 @@
+package br.com.thierryboiago.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+
+    println("--------------")
+    println("Maior salario: ${salarios.max()}")
+    println("Menor salario: ${salarios.min()}")
+    println("Media salarial: ${salarios.average()}")
+
+
+    println("--------------")
+    val salariosMaiorQue2500 = salarios.filter { it > 2500 }
+    salariosMaiorQue2500.forEach {
+        println(it)
+    }
+
+    println("--------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+
+    println("--------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+
+    println("--------------")
+    println(salarios.any() { it == 1000.0 })
+    println(salarios.any() { it == 500.0 })
+}
