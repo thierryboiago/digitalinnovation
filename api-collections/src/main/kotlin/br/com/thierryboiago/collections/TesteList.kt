0753a94d1838ea3412537a5794de76d4f17ba19e
Thierry <thierry123123@gmail.com>
2021-07-22 22:34:55 -0300

    criado Teste para List com o tipo da classe Funcionario

---
+++ api-collections/src/main/kotlin/br/com/thierryboiago/collections/TesteList.kt
@@ -0,0 +1,45 @@
+package br.com.thierryboiago.collections
+
+fun main() {
+    val funcionario1 = Funcionario("Joao", 2000.0,"CLT")
+    val funcionario2 = Funcionario("Pedro", 1500.0,"PJ")
+    val funcionario3 = Funcionario("Maria", 4000.0,"CLT")
+
+    val funcionarios = listOf(funcionario1,funcionario2,funcionario3)
+
+    funcionarios.forEach{ println(it) }
+
+
+    println("********************")
+    println(funcionarios.find { it.nome == "Maria" })
+
+
+
+    println("********************")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+
+    println("********************")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
+
+
+
+}
+
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+){
+    override fun toString(): String = """
+        
+        Nome:         $nome
+        Salário:      $salario
+    """.trimIndent()
+}
