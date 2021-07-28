59da095dd983e481592e8a24024bf29b81144685
Thierry <thierry123123@gmail.com>
2021-07-27 21:05:18 -0300

    criado a classeRepositorio para simular um banco de dados e o uso de mutableMapOf, e criado classe Teste

---
+++ api-collections/src/main/kotlin/br/com/thierryboiago/collections/Repositorio.kt
@@ -0,0 +1,13 @@
+package br.com.thierryboiago.collections
+
+class Repositorio<T> {
+    private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T){
+        map[id] = value
+    }
+
+    fun findById(id: String) = map[id]
+
+
+}
---
+++ api-collections/src/main/kotlin/br/com/thierryboiago/collections/TesteMutavelMap.kt
@@ -0,0 +1,15 @@
+package br.com.thierryboiago.collections
+
+fun main() {
+    val funcionario1 = Funcionario("Joao", 2000.0,"CLT")
+    val funcionario2 = Funcionario("Pedro", 1500.0,"PJ")
+    val funcionario3 = Funcionario("Maria", 4000.0,"CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(funcionario1.nome, funcionario1)
+    repositorio.create(funcionario2.nome, funcionario2)
+    repositorio.create(funcionario3.nome, funcionario3)
+
+    println(repositorio.findById(funcionario2.nome))
+}
